a4512afc6d7196d36533e6fae7b4f3642fed4351
Alex020202 <50020386+Alex020202@users.noreply.github.com>
2019-05-18 16:38:46 +0500

    Update Block.py

--- src/Block.py
+++ src/Block.py
@@ -1,19 +1,19 @@
 import pygame
-import random
 
 
 class Block:
-    def __init__(self, x, y, Check_del):
+    def __init__(self, x, y, Check_del, my_game):
         self.x = x
         self.y = y
         self.x_w = x + 30
         self.Check_del = Check_del
+        self.win = my_game.win
         if not self.Check_del:
             self.block_image = pygame.image.load("../assets/pictures/block.png")
         else:
             self.block_image = pygame.image.load("../assets/pictures/white_block.png")
 
-    def draw_block(self, my_game):
-        my_game.win.blit(self.block_image, (self.x, self.y))
+    def draw(self):
+        self.win.blit(self.block_image, (self.x, self.y))
 
 
